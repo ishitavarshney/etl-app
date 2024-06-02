95fe7c445e4025d51d76d298ec25b2eea3ad26f0
ishitavarshney <varshney.ishita99@gmail.com>
2024-06-02 17:51:46 +0530

    initial commit

---
+++ src/main/java/com/example/etl/EtlAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.etl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtlAppApplication.class, args);
+	}
+
+}
